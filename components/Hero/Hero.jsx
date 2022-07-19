f57ca7a86efa18d343f86e234b8bf6a3b39d9c65
success hycenth <successhycenth8@@gmail.com>
2022-07-19 08:11:12 +0100

    pushing sfan

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
     <div className="mt-20 relative w-full flex flex-col items-center justify-center mx-auto">
-      <div className="h-[700px] bg-center	 bg-fixed bg-no-repeat bg-cover bg-[url('/globe.gif')] md:bg-[url('/home.png')]  w-full"></div>
+      <div className="h-[700px] bg-center	relative bg-fixed bg-no-repeat bg-cover  md:bg-[url('/home.png')]  w-full">
+        <Image src="/globe.gif" objectFit="cover" alt="globe" layout="fill" />
+      </div>
       <div className="absolute text-white mx-10 left-0 right-0 md:top-52 z-10">
         <h1 className="text-start w-[200px] md:w-[500px] leading-tight font-black text-[40px] md:text-7xl">
           Build a Life, Not a Resume
