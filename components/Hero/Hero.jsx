ebcfdbb35f58f5ed81f25622973740ae1767ed99
success hycenth <successhycenth8@@gmail.com>
2022-07-19 08:22:58 +0100

    pushing sfan

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -5,7 +5,15 @@ const Hero = () => {
   return (
     <div className="mt-20 relative w-full flex flex-col items-center justify-center mx-auto">
       <div className="h-[700px] bg-center	relative bg-fixed bg-no-repeat bg-cover  md:bg-[url('/home.png')]  w-full">
-        <Image src="/globe.gif" objectFit="cover" alt="globe" layout="fill" />
+        <div className="md:hidden">
+          <Image
+            src="/globe.gif"
+            className="md:hidden"
+            objectFit="cover"
+            alt="globe"
+            layout="fill"
+          />
+        </div>
       </div>
       <div className="absolute text-white mx-10 left-0 right-0 md:top-52 z-10">
         <h1 className="text-start w-[200px] md:w-[500px] leading-tight font-black text-[40px] md:text-7xl">
