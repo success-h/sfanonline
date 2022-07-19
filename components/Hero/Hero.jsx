8a3c84dfffccb7a8750f7d061406b42a6747277b
success hycenth <successhycenth8@@gmail.com>
2022-07-19 07:51:06 +0100

    pushing sfan

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="mt-20 relative w-full flex flex-col items-center justify-center mx-auto">
-      <div className="h-[700px] bg-center	 bg-fixed bg-no-repeat bg-cover bg-[url('/globe.gif')] md:bg-[url('/home.png')]  w-full"></div>
+      <div className="h-[700px] bg-center	 bg-fixed bg-no-repeat bg-contain md:bg-cover bg-[url('/globe.gif')] md:bg-[url('/home.png')]  w-full"></div>
       <div className="absolute text-white mx-10 left-0 right-0 md:top-52 z-10">
         <h1 className="text-start w-[200px] md:w-[500px] leading-tight font-black text-[40px] md:text-7xl">
           Build a Life, Not a Resume
--- components/inspiration/inspiration.jsx
+++ components/inspiration/inspiration.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Inspiration = () => {
   return (
-    <div className="md:h-[400px] h-[300px] bg-center px-4 md:px-0	flex items-center justify-center bg-fixed bg-no-repeat bg-cover bg-[url('/globe.gif')] md:bg-[url('/inspiration.svg')]  w-full">
+    <div className="md:h-[400px] h-[300px] bg-center px-4 md:px-0	flex items-center justify-center bg-fixed bg-no-repeat bg-cover md:bg-cover bg-[url('/globe.gif')] md:bg-[url('/inspiration.svg')]  w-full">
       <div className="font-black text-2xl sm:text-2xl md:text-5xl text-center md:w-3/5 bg-clip-text bg-gradient-to-r from-sky-400 to-pink-400 drop-shadow-xl text-transparent ">
         We believe that young people are smart and if given the skills and
         opportunity to engage the real world, magic will happen.
